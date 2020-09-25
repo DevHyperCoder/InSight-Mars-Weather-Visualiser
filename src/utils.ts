42f2a3d7cc221de011bbb80f9045fce8bf446182
DevHyperCoder <arduinoleo88@gmail.com>
2020-09-25 11:51:31 +0530

    🏗 Add utils 1. Convert Celcius to Farenheit 2. Convert Farenheit to Celcius

---
+++ src/utils.ts
@@ -0,0 +1,8 @@
+export function convertFtoC(farenhietV: number) {
+    return ((farenhietV - 32) * 5 / 9)
+}
+
+export function convertCtoF(celcius: number) {
+    return ((celcius * 9 / 5) + 32)
+}
+
