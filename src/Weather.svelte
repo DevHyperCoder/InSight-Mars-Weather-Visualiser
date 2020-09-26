26e6a20f5e6913bf7fe2d74369e7349e0a8dd5dc
DevHyperCoder <arduinoleo88@gmail.com>
2020-09-26 14:11:26 +0530

    💄 Styling

--- src/Weather.svelte
+++ src/Weather.svelte
@@ -4,11 +4,18 @@
 </script>
 
 <style>
+  
+
   div {
     margin-top: 3rem;
+    height: 100%;
   }
-  h1 {
-    text-align: center;
+  .card {
+    max-width: 500px;
+    margin-left: 100px;
+    margin-top: 5rem;
+    box-shadow: 5px 10px aliceblue;
+    padding-bottom: 0.5rem;
   }
 </style>
 
@@ -17,9 +24,20 @@
     {#await data}
       <p>...waiting</p>
     {:then convertedData}
-      <h1>Current Season: {convertedData.Season}</h1>
-      <h1>Current windspeed: {convertedData.HWS.av}m/s</h1>
-      <h1>Temp: {convertedData.AT.av}F</h1>
+      <div class="card">
+        <h1>Current Season: {convertedData.Season}</h1>
+      </div>
+      <div class="card">
+        <h1>Winds</h1>
+        <h2>Average windspeed: {convertedData.HWS.av}m/s</h2>
+        <h2>Maximum windspeed: {convertedData.HWS.mx}m/s</h2>
+      </div>
+      <div class="card">
+        <h1>Temperatures</h1>
+        <h2>Minimum Temp: {convertedData.AT.mn}F</h2>
+        <h2>Average Temp: {convertedData.AT.av}F</h2>
+        <h2>Maximum Temp: {convertedData.AT.mx}F</h2>
+      </div>
     {/await}
   </div>
 </template>
